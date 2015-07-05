4cb106d0cbbe3560b1d5e7f67231c291c9ab10b5
Michal Michalczuk <michalczukm@gmail.com>
2015-07-06 01:00:59 +0200

    add pipe

---
+++ Middleware/BadassMiddleware.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
+
+namespace PizzaToDo.Middleware
+{
+    public class BadassMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public BadassMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            if (context.Request.HasFormContentType && context.Request.Form.Any() && context.Request.Form.Select(pair => string.Join(" ", pair.Value)).Contains("hack"))
+            {
+                context.Response.ContentType = "text/html";
+                context.Response.Redirect("/Home/Badass");
+            }
+
+            await _next(context);
+        }
+    }
+
+    public static class MiddlewareExtensions
+    {
+        public static IApplicationBuilder UseBadass(this IApplicationBuilder app)
+        {
+            return app.UseMiddleware<BadassMiddleware>();
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,25 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNet.Authentication.Facebook;
-using Microsoft.AspNet.Authentication.Google;
-using Microsoft.AspNet.Authentication.MicrosoftAccount;
-using Microsoft.AspNet.Authentication.Twitter;
 using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Diagnostics;
 using Microsoft.AspNet.Diagnostics.Entity;
 using Microsoft.AspNet.Hosting;
 using Microsoft.AspNet.Http;
-using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
-using Microsoft.AspNet.Routing;
 using Microsoft.Data.Entity;
 using Microsoft.Framework.ConfigurationModel;
 using Microsoft.Framework.DependencyInjection;
 using Microsoft.Framework.Logging;
-using Microsoft.Framework.Logging.Console;
-using Microsoft.Framework.Runtime;
 using PizzaToDo.Models;
 using PizzaToDo.Services;
 
@@ -66,21 +56,6 @@ namespace PizzaToDo
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
 
-            // Configure the options for the authentication middleware.
-            // You can add options for Google, Twitter and other middleware as shown below.
-            // For more information see http://go.microsoft.com/fwlink/?LinkID=532715
-            services.Configure<FacebookAuthenticationOptions>(options =>
-            {
-                options.AppId = Configuration["Authentication:Facebook:AppId"];
-                options.AppSecret = Configuration["Authentication:Facebook:AppSecret"];
-            });
-
-            services.Configure<MicrosoftAccountAuthenticationOptions>(options =>
-            {
-                options.ClientId = Configuration["Authentication:MicrosoftAccount:ClientId"];
-                options.ClientSecret = Configuration["Authentication:MicrosoftAccount:ClientSecret"];
-            });
-
             // Add MVC services to the services container.
             services.AddMvc();
 
@@ -117,12 +92,18 @@ namespace PizzaToDo
             // Add cookie-based authentication to the request pipeline.
             app.UseIdentity();
 
-            // Add authentication middleware to the request pipeline. You can configure options such as Id and Secret in the ConfigureServices method.
-            // For more information see http://go.microsoft.com/fwlink/?LinkID=532715
-            // app.UseFacebookAuthentication();
-            // app.UseGoogleAuthentication();
-            // app.UseMicrosoftAccountAuthentication();
-            // app.UseTwitterAuthentication();
+            app.Use(async (context, next) =>
+            {
+                if (context.Request.HasFormContentType && context.Request.Form.Any() && context.Request.Form.Select(pair => string.Join(" ", pair.Value)).Contains("hack"))
+                {
+                    context.Response.ContentType = "text/html";
+                    context.Response.Redirect("/Home/Badass");
+                }
+
+                await next();
+            });
+
+
 
             // Add MVC to the request pipeline.
             app.UseMvc(routes =>
